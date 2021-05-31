459ea4d5bc4d7fd8dd50094bcc18236689aeade9
Max Fyk <33779386+maxfyk@users.noreply.github.com>
2021-05-31 14:53:44 +0300

    p

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.8
+
+WORKDIR /usr/src/app
+ENV PYTHONUNBUFFERED = 1
+
+COPY requirements.txt requirements.txt
+RUN python3 -m pip install -r requirements.txt
+
+COPY . .
---
+++ api.py
@@ -0,0 +1,24 @@
+"""Parer api"""
+# not implemented yet
+import sys
+from flask import Blueprint
+from redis import Redis
+from rq import Worker, Queue, Connection
+
+def get_redis():
+    """Returns redis connection"""
+    try:
+        redis = Redis(host='redis', port=6379)
+    except Redis.DoesNotExist as error:
+        print(error)
+        sys.exit("Error: Faild connecting to redis")
+    return redis
+
+
+api = Blueprint('urls2', __name__, url_prefix="api")
+redis = get_redis()
+
+
+@api.route('/')
+def index():
+    return 'api is not implemented yet'
