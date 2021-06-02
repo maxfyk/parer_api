8ec5ab03bbdcfc200b7595a727140b0f9fbfacc7
max <maxfy@gmail.com>
2021-06-02 03:08:08 +0300

    from methods.connection import get_redis, get_cursor

--- api.py
+++ api.py
@@ -4,16 +4,7 @@ import sys
 from flask import Blueprint
 from redis import Redis
 from rq import Worker, Queue, Connection
-
-
-def get_redis():
-    """Returns redis connection"""
-    try:
-        redis = Redis(host='redis', port=6379)
-    except Redis.DoesNotExist as error:
-        print(error)
-        sys.exit("Error: Faild connecting to redis")
-    return redis
+from methods.connection import get_redis, get_cursor
 
 
 api = Blueprint('urls2', __name__, url_prefix="api")
